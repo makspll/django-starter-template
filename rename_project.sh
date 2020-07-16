8d38527219383ea09456a356ee54554f8b9178e0
makspll <makspl17@gmail.com>
2020-07-16 16:45:06 +0100

    fixed another error

--- rename_project.sh
+++ rename_project.sh
@@ -3,13 +3,13 @@
 if [ "$1" != "" ]; then
 
     NAME_VAL=$1
-    mv template_project $NAME_VAL
     declare -a FilesContainingProjectName=("manage.py" "procfile" ".gitignore" "template_project/urls.py" "template_project/wsgi.py" "template_project/asgi.py" "template_project/components/common.py")
      
     for file in ${FilesContainingProjectName[@]}; do
         sed -i "s/template_project/$NAME_VAL/g" $file
 
     done
+    mv template_project $NAME_VAL
 else
     echo "Project name is required"
 fi
