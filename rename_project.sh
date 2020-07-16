9c06deef9ba694173eb01a9d6ec144238abff89a
makspll <makspl17@gmail.com>
2020-07-16 16:44:32 +0100

    fixed error

--- rename_project.sh
+++ rename_project.sh
@@ -7,7 +7,7 @@ if [ "$1" != "" ]; then
     declare -a FilesContainingProjectName=("manage.py" "procfile" ".gitignore" "template_project/urls.py" "template_project/wsgi.py" "template_project/asgi.py" "template_project/components/common.py")
      
     for file in ${FilesContainingProjectName[@]}; do
-        sed -i "s/template_project/$NAME_VAL/g" file
+        sed -i "s/template_project/$NAME_VAL/g" $file
 
     done
 else
